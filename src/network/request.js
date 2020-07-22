efbf22fb5b3d5056e8e1be9b62a22809ad27e020
codejjt <2427489841@qq.com>
2020-07-22 14:57:03 +0800

    to update

--- src/network/request.js
+++ src/network/request.js
@@ -4,8 +4,8 @@ export function request(config) {
 
   //创建axios实例
   const instance = axios.create({
-    // baseURL: "http://123.207.32.32:8000",
-    baseURL: "http://152.136.185.210:8000/api/n3",
+    baseURL: "http://123.207.32.32:8000",
+
     timeout: 5000
   })
 
